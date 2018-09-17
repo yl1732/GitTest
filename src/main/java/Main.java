8d8e93447d22aa2afd0cff877e87bec5c25f0c39
yl1732 <houwq@yealink.com>
2018-09-17 20:17:49 +0800

    master B

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @Des Main;
+ * @Author Seakeer;
+ * @Date 2018/9/17;
+ * @Other
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Git Test");
+    }
+}
